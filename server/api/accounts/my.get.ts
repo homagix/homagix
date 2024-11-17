cbae91d680f3d7b1e7f1e82a951261cf743c7714
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2024-11-17 18:39:17 +0100

    fix: prevent top level await

--- server/api/accounts/my.get.ts
+++ server/api/accounts/my.get.ts
@@ -3,9 +3,8 @@ import { getAuthenticatedUser } from "~/server/AuthHelper"
 import { useFavorites } from "~/server/model/Favorites"
 import { useUsers } from "~/server/model/Users"
 
-const { getReadableUserFields } = await useUsers()
-
 export default defineEventHandler(async event => {
+  const { getReadableUserFields } = await useUsers()
   const user = await getAuthenticatedUser(event)
   const favorites = useFavorites(user)
 
